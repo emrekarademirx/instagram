2c5fb68f4b2f8626c6b93a72592feff6b2f4828d
Emre Karademir <99919329+emrekarademirx@users.noreply.github.com>
2023-02-22 20:55:46 +0300

    Add files via upload

--- public/script.js
+++ public/script.js
@@ -1,1 +1,32 @@
+/* Takip etmeyenleri takipten çıkarma işlemi */
+async function unfollowNonFollowers() {
+  const response = await fetch('/unfollow', { method: 'POST' });
+  const result = await response.json();
+  alert(result.message);
+}
 
+/* Beğeni işlemi */
+async function likePosts() {
+  const response = await fetch('/like', { method: 'POST' });
+  const result = await response.json();
+  alert(result.message);
+}
+
+/* Ana kod bloğu */
+document.addEventListener('DOMContentLoaded', () => {
+  // Takip etmeyenleri takipten çıkarma butonu
+  const unfollowButton = document.querySelector('#unfollow-button');
+  if (unfollowButton) {
+    unfollowButton.addEventListener('click', () => {
+      unfollowNonFollowers();
+    });
+  }
+
+  // Beğeni butonu
+  const likeButton = document.querySelector('#like-button');
+  if (likeButton) {
+    likeButton.addEventListener('click', () => {
+      likePosts();
+    });
+  }
+});
